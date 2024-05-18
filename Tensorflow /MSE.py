55443100d47c00a2a256553db197ec18670c3b4d
謝爾頓·庫珀 <119040957+Rohanberiwal@users.noreply.github.com>
2024-05-18 10:05:49 +0530

    Create MSE.py
    Mean squared Error for the predicted and the actual values  from the NN model .

---
+++ Tensorflow /MSE.py
@@ -0,0 +1,24 @@
+import tensorflow as tf 
+## This is a single layer  Neural network 
+## layer init by using the unit funtion that creates more than one precptorns for the NN 
+n = 10 ## 10 hidden layer dense one
+predicted =  input()##Output from the model  
+
+loss = tf.reduce_mean(tf.square(tf.subtract(y,predicted)))
+loss =  tf.keras_losses.MSE(y,predicted) 
+
+model  = tf.keras.Sequential([tf.keras.layers.dense(n) , tf.keras.layers.dense(2)])
+layer =  tf.keras.layers.Dense(unit =2)
+class Layer(tf.keras.layers.Layer) :
+    def __init__(self , input_dim ,  output_dim)  :
+        super(Layer, self).__init__()
+        self.W = self.add_weights([input_dim  , output_dim])
+        self.b = self.add_weights([1,output_dim])
+        
+    def call(self, inputs) :
+        z = tf.matmul(inputs , self.W) + self.b
+        ##Non linear function conversion 
+        output =  tf.math.sigmoid(z) 
+        return output 
+    
+    

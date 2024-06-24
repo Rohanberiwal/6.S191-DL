18d2e3abff23a944270b1a0bd881f53c6fe42c33
HyperNova x14 <119040957+Rohanberiwal@users.noreply.github.com>
2024-06-24 17:56:32 +0530

    Create FineTunedFasterRCNN.py

---
+++ Tensorflow /Faster RCNN/FineTunedFasterRCNN.py
@@ -0,0 +1,245 @@
+import os
+import json
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+standard_dict_mitotic = {}
+standard_dict_non_mitotic = {}
+
+import os
+import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+import numpy as np
+
+class CustomDataset(Dataset):
+    def __init__(self, mitotic_dict, non_mitotic_dict, transforms=None):
+        self.mitotic_dict = mitotic_dict
+        self.non_mitotic_dict = non_mitotic_dict
+        self.transforms = transforms
+        self.image_files = list(set(mitotic_dict.keys()).union(set(non_mitotic_dict.keys())))
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_path = self.image_files[idx]
+        img = Image.open(img_path).convert("RGB")
+        
+        boxes = []
+        labels = []
+
+        if img_path in self.mitotic_dict:
+            for box in self.mitotic_dict[img_path]:
+                boxes.append(box)
+                labels.append(1)  # Mitotic label
+
+        if img_path in self.non_mitotic_dict:
+            for box in self.non_mitotic_dict[img_path]:
+                boxes.append(box)
+                labels.append(0)  # Non-mitotic label
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        # Filter out invalid boxes
+        valid_indices = (boxes[:, 2] > boxes[:, 0]) & (boxes[:, 3] > boxes[:, 1])
+        boxes = boxes[valid_indices]
+        labels = labels[valid_indices]
+
+        if self.transforms:
+            img = self.transforms(img)
+
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        target['image_id'] = torch.tensor([idx])
+        target['area'] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        target['iscrowd'] = torch.zeros((len(boxes),), dtype=torch.int64)
+
+        return img, target
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+def extract_bounding_box(filename, path_to_file, mitotic_annotation_file, non_mitotic_annotation_file):
+    if mitotic_annotation_file in path_to_file:
+        annotation_file = mitotic_annotation_file
+    elif non_mitotic_annotation_file in path_to_file:
+        annotation_file = non_mitotic_annotation_file
+    else:
+        raise ValueError("File not found in either mitotic or non-mitotic annotation files.")
+
+    with open(annotation_file) as f:
+        annotations = json.load(f)
+
+    if filename in annotations:
+        annotation = annotations[filename]
+        boxes = []
+        for region in annotation['regions']:
+            shape_attr = region['shape_attributes']
+            x = shape_attr['x']
+            y = shape_attr['y']
+            width = shape_attr['width']
+            height = shape_attr['height']
+            boxes.append([x, y, x + width, y + height])
+        return boxes
+    else:
+        raise ValueError(f"Filename '{filename}' not found in '{annotation_file}'.")
+
+def print_mitotic(json_mitotic):
+    print("This is the mitotic printer function")
+    universal_list = []
+    with open(json_mitotic, 'r') as f:
+        data = json.load(f)
+    for image_key, image_data in data.items():
+        filename = image_data.get('filename', 'Unknown')
+        print(f"File Name: {filename}")
+        boundary_box = []
+        for region in image_data.get('regions', []):
+            shape_attributes = region.get('shape_attributes', {})
+            xmin = shape_attributes.get('x', 'N/A')
+            ymin = shape_attributes.get('y', 'N/A')
+            width = shape_attributes.get('width', 'N/A')
+            height = shape_attributes.get('height', 'N/A')
+
+            print(f"Bounding Box Coordinates: xmin={xmin}, ymin={ymin}, width={width}, height={height}")
+            boundary_box.append(xmin)
+            boundary_box.append(ymin)
+            boundary_box.append(width)
+            boundary_box.append(height)
+            universal_list.append(boundary_box)
+            boundary_box = []
+        print("------------------------")
+        standard_dict_mitotic[filename.replace('.jpg', '.jpeg')] = universal_list
+        universal_list = []
+        boundary_box = []
+    print(universal_list)
+    return standard_dict_mitotic
+
+def print_filename_bbox(json_file):
+    print("This is the printer filename function for non-mitotic")
+    universal_list = []
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    for image_key, image_data in data.items():
+        filename = image_data.get('filename', 'Unknown')
+        print(f"File Name: {filename}")
+        boundary_box = []
+        for region in image_data.get('regions', []):
+            shape_attributes = region.get('shape_attributes', {})
+            xmin = shape_attributes.get('x', 'N/A')
+            ymin = shape_attributes.get('y', 'N/A')
+            width = shape_attributes.get('width', 'N/A')
+            height = shape_attributes.get('height', 'N/A')
+
+            print(f"Bounding Box Coordinates: xmin={xmin}, ymin={ymin}, width={width}, height={height}")
+            boundary_box.append(xmin)
+            boundary_box.append(ymin)
+            boundary_box.append(width)
+            boundary_box.append(height)
+            universal_list.append(boundary_box)
+            boundary_box = []
+        print("------------------------")
+        # Change .jpg to .jpeg in the dictionary key
+        standard_dict_non_mitotic[filename.replace('.jpg', '.jpeg')] = universal_list
+        universal_list = []
+        boundary_box = []
+    print(universal_list)
+    return standard_dict_non_mitotic
+
+def extract_filenames_from_json(json_file, root):
+    with open(json_file) as f:
+        data = json.load(f)
+    
+    filename_list = []
+    for filename, attributes in data.items():
+        filename = filename.replace('.jpeg', '.jpg')
+        img_name = attributes['filename'] 
+        img_path = os.path.join(root, img_name)  
+        file_path  = img_path
+        filename_list.append(img_path)
+        
+    return filename_list
+
+def modify_dict_inplace(standard_dict, root):
+    keys_to_remove = []
+    keys_to_add = []
+    
+    for key in standard_dict.keys():
+        key_new = key.replace('.jpeg', '.jpg')
+        image_key = os.path.join(root, key_new)
+        
+        keys_to_remove.append(key)
+        keys_to_add.append(image_key)
+    
+    for old_key, new_key in zip(keys_to_remove, keys_to_add):
+        standard_dict[new_key] = standard_dict.pop(old_key)
+
+# Define root and annotation files
+root = r"C:\Users\rohan\OneDrive\Desktop\Train_mitotic"
+mitotic_annotation_file = 'mitotic.json'
+non_mitotic_annotation_file = 'NonMitotic.json'
+
+# Extract filenames and modify dicts
+mitotic_filenames = extract_filenames_from_json(mitotic_annotation_file, root)
+mitotic_output = print_mitotic(mitotic_annotation_file)
+non_mitotic_output = print_filename_bbox(non_mitotic_annotation_file)
+
+modify_dict_inplace(standard_dict_non_mitotic, root)
+modify_dict_inplace(standard_dict_mitotic, root)
+
+# Create dataset and data loader
+dataset = CustomDataset(standard_dict_mitotic, standard_dict_non_mitotic, transforms=get_transform(train=True))
+data_loader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+# Load a pre-trained model
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+# Replace the classifier head
+num_classes = 2  # 1 class (object) + background
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+# Define training parameters
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model.to(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9, weight_decay=0.0005)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    model.train()
+    for images, targets in data_loader:
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        print(f'Epoch: {epoch}, Loss: {losses.item()}')
+
+    lr_scheduler.step()
+
+print("FASTER RCNN fine trained properly")
+
+
+

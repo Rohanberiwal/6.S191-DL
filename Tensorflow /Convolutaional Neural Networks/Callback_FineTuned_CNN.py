c1ffda0eaf2f1490dd76c3f3e7ab7cc1c540bf63
HyperNova x14 <119040957+Rohanberiwal@users.noreply.github.com>
2024-06-25 17:34:13 +0530

    Create Callback_FineTuned_CNN.py

---
+++ Tensorflow /Convolutaional Neural Networks/Callback_FineTuned_CNN.py
@@ -0,0 +1,431 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+import numpy as np
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import json
+import os
+import json
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+standard_dict_mitotic = {}
+standard_dict_non_mitotic = {}
+import os
+import json
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+import matplotlib.pyplot as plt
+import os
+import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+import numpy as np
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import json
+import os
+import json
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+standard_dict_mitotic = {}
+standard_dict_non_mitotic = {}
+import os
+import json
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+import matplotlib.pyplot as plt
+import os
+import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.ops as ops
+import cv2
+import numpy as np
+import json
+from torchvision.models import resnet50
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision.transforms as transforms
+import torch
+from torch.autograd import Function
+import pdb
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+import torch
+import torch.nn.functional as F
+import torchvision.ops as ops
+
+import torch
+import torch.nn.functional as F
+import torchvision.ops as ops
+
+import torch
+import torch.nn.functional as F
+import torchvision.ops as ops
+
+import torch
+import torch.nn.functional as F
+import torchvision.ops as ops
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+def extract_features(image_path):
+
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (224, 224))
+    image = preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+    features = base_model.predict(image)
+    return features
+
+def extract_bounding_box(filename, path_to_file, mitotic_annotation_file, non_mitotic_annotation_file):
+    if mitotic_annotation_file in path_to_file:
+        annotation_file = mitotic_annotation_file
+    elif non_mitotic_annotation_file in path_to_file:
+        annotation_file = non_mitotic_annotation_file
+    else:
+        raise ValueError("File not found in either mitotic or non-mitotic annotation files.")
+
+    with open(annotation_file) as f:
+        annotations = json.load(f)
+
+    if filename in annotations:
+        annotation = annotations[filename]
+        boxes = []
+        for region in annotation['regions']:
+            shape_attr = region['shape_attributes']
+            x = shape_attr['x']
+            y = shape_attr['y']
+            width = shape_attr['width']
+            height = shape_attr['height']
+            boxes.append([x, y, x + width, y + height])
+        return boxes
+    else:
+        raise ValueError(f"Filename '{filename}' not found in '{annotation_file}'.")
+
+def print_mitotic(json_mitotic):
+    print("This is the mitotic printer function")
+    universal_list = []
+    with open(json_mitotic, 'r') as f:
+        data = json.load(f)
+    for image_key, image_data in data.items():
+        filename = image_data.get('filename', 'Unknown')
+        print(f"File Name: {filename}")
+        boundary_box = []
+        for region in image_data.get('regions', []):
+            shape_attributes = region.get('shape_attributes', {})
+            xmin = shape_attributes.get('x', 'N/A')
+            ymin = shape_attributes.get('y', 'N/A')
+            width = shape_attributes.get('width', 'N/A')
+            height = shape_attributes.get('height', 'N/A')
+
+            print(f"Bounding Box Coordinates: xmin={xmin}, ymin={ymin}, width={width}, height={height}")
+            boundary_box.append(xmin)
+            boundary_box.append(ymin)
+            boundary_box.append(width)
+            boundary_box.append(height)
+            universal_list.append(boundary_box)
+            boundary_box = []
+        print("------------------------")
+        standard_dict_mitotic[filename.replace('.jpg', '.jpeg')] = universal_list
+        universal_list = []
+        boundary_box = []
+    print(universal_list)
+    return standard_dict_mitotic
+
+def print_filename_bbox(json_file):
+    print("This is the printer filename function for non-mitotic")
+    universal_list = []
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    for image_key, image_data in data.items():
+        filename = image_data.get('filename', 'Unknown')
+        print(f"File Name: {filename}")
+        boundary_box = []
+        for region in image_data.get('regions', []):
+            shape_attributes = region.get('shape_attributes', {})
+            xmin = shape_attributes.get('x', 'N/A')
+            ymin = shape_attributes.get('y', 'N/A')
+            width = shape_attributes.get('width', 'N/A')
+            height = shape_attributes.get('height', 'N/A')
+
+            print(f"Bounding Box Coordinates: xmin={xmin}, ymin={ymin}, width={width}, height={height}")
+            boundary_box.append(xmin)
+            boundary_box.append(ymin)
+            boundary_box.append(width)
+            boundary_box.append(height)
+            universal_list.append(boundary_box)
+            boundary_box = []
+        print("------------------------")
+        # Change .jpg to .jpeg in the dictionary key
+        standard_dict_non_mitotic[filename.replace('.jpg', '.jpeg')] = universal_list
+        universal_list = []
+        boundary_box = []
+    print(universal_list)
+    return standard_dict_non_mitotic
+
+
+def extract_bounding_boxes(annotation_file, root_dir):
+    with open(annotation_file, 'r') as f:
+        data = json.load(f)
+    
+    annotations = {}
+    for filename, attributes in data.items():
+        img_name = attributes['filename']  
+        img_path = os.path.join(root_dir, img_name)
+        boxes = []
+        for region in attributes['regions']:
+            shape_attr = region['shape_attributes']
+            x = shape_attr['x']
+            y = shape_attr['y']
+            width = shape_attr['width']
+            height = shape_attr['height']
+            boxes.append([x, y, x + width, y + height])
+        annotations[img_path] = boxes
+    
+    return annotations
+
+
+transform = T.Compose([T.Resize((224, 224)), T.ToTensor()])
+
+def tensor_image(image):
+    # Convert PIL image to PyTorch tensor with channels-last format (C x H x W)
+    image = T.ToTensor()(image)
+    return image
+
+def load_images_and_annotations(root_dir, mitotic_dict, non_mitotic_dict):
+    X = []
+    y = []
+    for filename, mitotic_boxes in mitotic_dict.items():
+        img_path = os.path.join(root_dir, filename)
+        img = Image.open(img_path).convert('RGB')
+        img = img.resize((224, 224))  # Resize image to (224, 224)
+        img = np.array(img)  # Convert PIL.Image to numpy array
+        X.append(img)
+        y.append(1)  # Mitotic class label
+        
+    for filename, non_mitotic_boxes in non_mitotic_dict.items():
+        img_path = os.path.join(root_dir, filename)
+        img = Image.open(img_path).convert('RGB')
+        img = img.resize((224, 224))  # Resize image to (224, 224)
+        img = np.array(img)  # Convert PIL.Image to numpy array
+        X.append(img)
+        y.append(0)  # Non-mitotic class label
+        
+    return np.array(X), np.array(y)
+def extract_filenames_from_json(json_file, root):
+    with open(json_file) as f:
+        data = json.load(f)
+    
+    filename_list = []
+    for filename, attributes in data.items():
+        filename = filename.replace('.jpeg', '.jpg')
+        img_name = attributes['filename'] 
+        img_path = os.path.join(root, img_name)  
+        file_path  = img_path
+        filename_list.append(img_path)
+        
+    return filename_list
+
+def modify_dict_inplace(standard_dict, root):
+    keys_to_remove = []
+    keys_to_add = []
+    
+    for key in standard_dict.keys():
+        key_new = key.replace('.jpeg', '.jpg')
+        image_key = os.path.join(root, key_new)
+        
+        keys_to_remove.append(key)
+        keys_to_add.append(image_key)
+    
+    for old_key, new_key in zip(keys_to_remove, keys_to_add):
+        standard_dict[new_key] = standard_dict.pop(old_key)
+
+# Define root and annotation files
+root = r"C:\Users\rohan\OneDrive\Desktop\Train_mitotic"
+mitotic_annotation_file = 'mitotic.json'
+non_mitotic_annotation_file = 'NonMitotic.json'
+
+# Extract filenames and modify dicts
+mitotic_filenames = extract_filenames_from_json(mitotic_annotation_file, root)
+mitotic_output = print_mitotic(mitotic_annotation_file)
+non_mitotic_output = print_filename_bbox(non_mitotic_annotation_file)
+
+modify_dict_inplace(standard_dict_non_mitotic, root)
+modify_dict_inplace(standard_dict_mitotic, root)
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+
+
+# Define root directory and annotation files
+root = r"C:\Users\rohan\OneDrive\Desktop\Train_mitotic"
+mitotic_annotation_file = 'mitotic.json'
+non_mitotic_annotation_file = 'NonMitotic.json'
+
+# Load annotations
+standard_dict_mitotic = print_mitotic(mitotic_annotation_file)
+standard_dict_non_mitotic = print_filename_bbox(non_mitotic_annotation_file)
+
+# Modify dictionaries inplace
+modify_dict_inplace(standard_dict_non_mitotic, root)
+modify_dict_inplace(standard_dict_mitotic, root)
+
+# Load images and annotations
+X, y = load_images_and_annotations(root, standard_dict_mitotic, standard_dict_non_mitotic)
+print(X)
+print(y)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Data augmentation
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+X_train, X_val = np.zeros((100, 224, 224, 3)), np.zeros((20, 224, 224, 3))  # Placeholder data, replace with actual loading
+y_train, y_val = np.zeros((100,)), np.zeros((20,))  # Placeholder labels, replace with actual loading
+
+
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+batch_size = 4
+steps_per_epoch = len(X_train) // batch_size
+epochs = 10
+
+test_img_path = r"C:\Users\rohan\OneDrive\Desktop\A00_01.jpg"
+
+
+# Adjusting Dense layer units and activation
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu')(x)  # Experiment with different units
+predictions = Dense(1, activation='sigmoid')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+# Adjusting learning rate and optimizer
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)  # Experiment with different learning rates
+model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+filepath = 'best_model.keras'  # Ensure the filepath ends with .keras
+
+# Define callbacks including ModelCheckpoint with the correct filepath
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=3),
+    tf.keras.callbacks.ModelCheckpoint(filepath, monitor='val_accuracy', save_best_only=True, verbose=1),
+    tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1)
+]
+# Training the model with callbacks
+model.fit(
+    train_datagen.flow(X_train, y_train, batch_size=batch_size),
+    steps_per_epoch=steps_per_epoch,
+    epochs=epochs,
+    validation_data=(X_val, y_val),
+    callbacks=callbacks
+)
+
+# Evaluating model performance
+loss, accuracy = model.evaluate(X_val, y_val)
+print(f"Validation accuracy: {accuracy * 100:.2f}%")
+
+history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=10, batch_size=32)
+
+# Accessing training history
+print(history.history.keys())
+plt.plot(history.history['accuracy'], label='training accuracy')
+plt.plot(history.history['val_accuracy'], label='validation accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+
+
+print("****** CNN Layer ********\n")
+features = extract_features(test_img_path)
+print(features)
+
+print("\n****** CNN Layer ********\n")

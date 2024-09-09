d57a074ddb9765c4cd3d651d0a2845e43cfe14c3
~ZephyrOfLight <119040957+Rohanberiwal@users.noreply.github.com>
2024-09-09 14:51:10 +0530

    Create Swim_transformer_with_predicted_results.py
    This is is the swin transformer that has print the results of the valdation side by side .

---
+++ Latest_Updates/Swin_Transfomer/Swim_transformer_with_predicted_results.py
@@ -0,0 +1,481 @@
+import timm
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+torch.cuda.empty_cache()
+model_name = 'swin_tiny_patch4_window7_224'
+model = timm.create_model(model_name, pretrained=True)
+
+import os
+import os
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import zipfile
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision.transforms as T
+
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import torchvision.transforms as T
+
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as F
+from PIL import Image
+import torchvision.transforms as T
+import matplotlib.pyplot as plt
+import os
+import json
+from PIL import Image, ImageDraw
+import random
+import torch
+from torchvision.io import read_image
+from torchvision.transforms import functional as F
+from PIL import Image
+import torch
+import torchvision
+import timm
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from PIL import Image
+from torchvision import transforms
+import random
+import torch
+from torchvision import transforms
+from PIL import Image
+from torchvision.transforms import functional as F
+from sklearn.model_selection import train_test_split
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+def extract_bounding_box(filename, path_to_file, mitotic_annotation_file, non_mitotic_annotation_file):
+    if mitotic_annotation_file in path_to_file:
+        annotation_file = mitotic_annotation_file
+    elif non_mitotic_annotation_file in path_to_file:
+        annotation_file = non_mitotic_annotation_file
+    else:
+        raise ValueError("File not found in either mitotic or non-mitotic annotation files.")
+
+    with open(annotation_file) as f:
+        annotations = json.load(f)
+
+    if filename in annotations:
+        annotation = annotations[filename]
+        boxes = []
+        for region in annotation['regions']:
+            shape_attr = region['shape_attributes']
+            x = shape_attr['x']
+            y = shape_attr['y']
+            width = shape_attr['width']
+            height = shape_attr['height']
+            boxes.append([x, y, x + width, y + height])
+        return boxes
+    else:
+        raise ValueError(f"Filename '{filename}' not found in '{annotation_file}'.")
+
+
+def print_mitotic(json_mitotic):
+    print("Printing mitotic information:")
+    standard_dict_mitotic = {}
+    universal_list = []
+    with open(json_mitotic, 'r') as f:
+        data = json.load(f)
+    for image_key, image_data in data.items():
+        filename = image_data.get('filename', 'Unknown')
+        print(f"File Name: {filename}")
+        boundary_box = []
+        for region in image_data.get('regions', []):
+            shape_attributes = region.get('shape_attributes', {})
+            xmin = shape_attributes.get('x', 'N/A')
+            ymin = shape_attributes.get('y', 'N/A')
+            width = shape_attributes.get('width', 'N/A')
+            height = shape_attributes.get('height', 'N/A')
+
+            print(f"Bounding Box Coordinates: xmin={xmin}, ymin={ymin}, width={width}, height={height}")
+            boundary_box.append([xmin, ymin, width, height])
+            universal_list.append([xmin, ymin, width, height])
+        print("------------------------")
+        standard_dict_mitotic[filename.replace('.jpg', '.jpeg')] = universal_list
+        universal_list = []
+        boundary_box = []
+    return standard_dict_mitotic
+
+
+def print_non_mitotic(json_non_mitotic):
+    print("Printing non-mitotic information:")
+    standard_dict_non_mitotic = {}
+    universal_list = []
+    with open(json_non_mitotic, 'r') as f:
+        data = json.load(f)
+    for image_key, image_data in data.items():
+        filename = image_data.get('filename', 'Unknown')
+        print(f"File Name: {filename}")
+        boundary_box = []
+        for region in image_data.get('regions', []):
+            shape_attributes = region.get('shape_attributes', {})
+            xmin = shape_attributes.get('x', 'N/A')
+            ymin = shape_attributes.get('y', 'N/A')
+            width = shape_attributes.get('width', 'N/A')
+            height = shape_attributes.get('height', 'N/A')
+
+            print(f"Bounding Box Coordinates: xmin={xmin}, ymin={ymin}, width={width}, height={height}")
+            boundary_box.append([xmin, ymin, width, height])
+            universal_list.append([xmin, ymin, width, height])
+        print("------------------------")
+        standard_dict_non_mitotic[filename.replace('.jpg', '.jpeg')] = universal_list
+        universal_list = []
+        boundary_box = []
+    return standard_dict_non_mitotic
+
+
+def extract_filenames_from_json(json_file, root):
+    with open(json_file) as f:
+        data = json.load(f)
+
+    filename_list = []
+    for filename, attributes in data.items():
+        filename = filename.replace('.jpeg', '.jpg')
+        img_name = attributes['filename']
+        img_path = os.path.join(root, img_name)
+        filename_list.append(img_path)
+
+    return filename_list
+
+
+
+def extract_filenames_from_json(json_file, root):
+    with open(json_file) as f:
+        data = json.load(f)
+
+    filename_list = []
+    for filename, attributes in data.items():
+        filename = filename.replace('.jpeg', '.jpg')
+        img_name = attributes['filename']
+        img_path = os.path.join(root, img_name)
+        filename_list.append(img_path)
+
+    return filename_list
+
+
+
+def plot_image(image_dict, title="Images"):
+    for image_path, boxes in image_dict.items():
+        try:
+            print(f"Trying to open image: {image_path}")
+            # Open the image
+            img = Image.open(image_path).convert("RGB")
+            draw = ImageDraw.Draw(img)
+
+            for box in boxes:
+                x, y, width, height = box
+                # Draw bounding box
+                draw.rectangle([x, y, x + width, y + height], outline="black", width=3)
+
+            img.show(title)
+
+        except FileNotFoundError:
+            print(f"File not found: {image_path}")
+            continue
+
+def modify_dict_inplace(standard_dict, root):
+    keys_to_remove = []
+    keys_to_add = []
+
+    for key in standard_dict.keys():
+        key_new = key.replace('.jpeg', '.jpg')
+        image_key = os.path.join(root, key_new)
+
+        keys_to_remove.append(key)
+        keys_to_add.append(image_key)
+
+    for old_key, new_key in zip(keys_to_remove, keys_to_add):
+        standard_dict[new_key] = standard_dict.pop(old_key)
+
+
+
+def extract_zip_file(input_zip_path, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with zipfile.ZipFile(input_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+    print(f"Files extracted to {output_dir}")
+
+
+import os
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+
+
+dataset_list = []
+
+def convert_bbox_xywh_to_minmax(bbox):
+    x, y, w, h = bbox
+    x_min = x
+    y_min = y
+    x_max = x + w
+    y_max = y + h
+    return [x_min, y_min, x_max, y_max]
+
+
+def convert_and_save_bounding_boxes(standard_dict, output_img_dir, output_txt_dir):
+    if not os.path.exists(output_img_dir):
+        os.makedirs(output_img_dir)
+    if not os.path.exists(output_txt_dir):
+        os.makedirs(output_txt_dir)
+
+    for img_path, bboxes in standard_dict.items():
+        img = Image.open(img_path)
+        img_width, img_height = img.size
+        img_filename = os.path.basename(img_path)
+        base_name, _ = os.path.splitext(img_filename)
+
+        # Save cropped image patches without bounding boxes
+        bbox_labels = []
+        image_patches_info = []
+
+        for i, bbox in enumerate(bboxes):
+            bbox_minmax = convert_bbox_xywh_to_minmax(bbox)
+            x_min, y_min, x_max, y_max = bbox_minmax
+
+            # Save cropped image patch
+            cropped_img = img.crop((x_min, y_min, x_max, y_max))
+            cropped_img_filename = f"{base_name}_bbox_{i}.png"
+            cropped_img_path = os.path.join(output_img_dir, cropped_img_filename)
+            cropped_img.save(cropped_img_path)
+
+            bbox_label = f"{x_min} {y_min} {x_max} {y_max}"
+            label = 1
+            bbox_labels.append((bbox_label, label))
+
+            image_patches_info.append({
+                'path': cropped_img_path,
+                'bbox': bbox_label,
+                'label': label
+            })
+
+        # Save bounding box labels to text file
+        txt_filename = f"{base_name}.txt"
+        txt_path = os.path.join(output_txt_dir, txt_filename)
+        with open(txt_path, 'w') as f:
+            for bbox_label, label in bbox_labels:
+                f.write(f"{bbox_label}\n{label}\n")
+
+        dataset_list.extend(image_patches_info)
+
+    return standard_dict
+
+
+input_zip_path_train = '/content/Train_mitotic.zip'
+output_dir_train = '/content/Train_mitotic'
+extract_zip_file(input_zip_path_train, output_dir_train)
+
+input_zip_path_tester= '/content/tester.zip'
+output_dir_tester = '/content/tester'
+extract_zip_file(input_zip_path_tester, output_dir_tester)
+
+root = '/content/Train_mitotic/Train_mitotic'
+mitotic_annotation_file = 'mitotic.json'
+standard_dict_mitotic = print_mitotic(mitotic_annotation_file)
+modify_dict_inplace(standard_dict_mitotic, root)
+
+print(standard_dict_mitotic)
+#plot_image(standard_dict_mitotic)
+
+train_output =  '/content/Faster_RCNN/patch'
+train_labeled = '/content/Faster_RCNN/patch_contents'
+updated_dict = convert_and_save_bounding_boxes(standard_dict_mitotic, train_output, train_labeled)
+
+print(updated_dict)
+print(dataset_list)
+
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+from torchvision.transforms import functional as F
+
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+import random
+from torchvision import transforms
+from torchvision.transforms import functional as F
+class CustomDataset(Dataset):
+    def __init__(self, data_list, transforms=None):
+        self.data_list = data_list
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        data = self.data_list[idx]
+        img_path = data['path']
+        image = Image.open(img_path).convert("RGB")
+
+        label = data['label']
+
+        if self.transforms:
+            image = self.transforms(image)
+        else:
+            image = transforms.ToTensor()(image)
+
+        return image, label
+
+
+
+def collate_fn(batch):
+    images, labels = zip(*batch)  # Unzip the batch
+    images = torch.stack(images)  # Stack images into a single tensor
+    labels = torch.tensor(labels, dtype=torch.long)  # Convert labels to a tensor
+    return images, labels
+
+# Model Setup for Swin Transformer
+def get_swin_transformer(num_classes):
+    model = timm.create_model('swin_base_patch4_window7_224', pretrained=True)
+    model.head = torch.nn.Linear(model.head.in_features, num_classes)  # Adjust the final layer
+    return model
+
+# Training Function
+def train_one_epoch(model, data_loader, optimizer, device):
+    model.train()
+    running_loss = 0.0
+
+    for images, labels in tqdm(data_loader, desc="Training Epoch"):
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = torch.nn.functional.cross_entropy(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    epoch_loss = running_loss / len(data_loader)
+    return epoch_loss
+
+# Evaluation Function
+def evaluate(model, data_loader, device):
+    model.eval()
+    running_loss = 0.0
+    predictions = []
+    true_labels = []
+    images_list = []
+    
+    with torch.no_grad():
+        for images, labels in tqdm(data_loader, desc="Evaluating"):
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            outputs = model(images)
+            loss = torch.nn.functional.cross_entropy(outputs, labels)
+            
+            running_loss += loss.item()
+            
+            # Collect predictions and true labels
+            preds = torch.argmax(outputs, dim=1).cpu().numpy()
+            predictions.extend(preds)
+            true_labels.extend(labels.cpu().numpy())
+            images_list.extend(images.cpu())
+    
+    epoch_loss = running_loss / len(data_loader)
+    
+    # Visualize predictions and true labels
+    visualize_predictions(images_list, predictions, true_labels)
+    
+    return epoch_loss
+
+def visualize_predictions(images, predictions, true_labels):
+    plt.figure(figsize=(15, 15))
+    num_images = len(predictions)
+    
+    for i in range(num_images):
+        plt.subplot(1, num_images, i + 1)
+        img = images[i].permute(1, 2, 0).numpy()  # Convert tensor to numpy array
+        plt.imshow(img)
+        plt.title(f"Pred: {predictions[i]}\nTrue: {true_labels[i]}")
+        plt.axis('off')
+    
+    plt.show()
+
+# Training Loop with loss tracking
+def train(model, train_loader, val_loader, num_epochs, device):
+    model.to(device)
+    
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = AdamW(params, lr=0.0001)
+
+    train_losses = []
+    val_losses = []
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+        train_loss = train_one_epoch(model, train_loader, optimizer, device)
+        print(f"Training Loss: {train_loss:.4f}")
+        train_losses.append(train_loss)
+
+        val_loss = evaluate(model, val_loader, device)
+        print(f"Validation Loss: {val_loss:.4f}")
+        val_losses.append(val_loss)
+
+    # Print loss per epoch
+    print("\nTraining Loss per Epoch:")
+    for i, loss in enumerate(train_losses):
+        print(f"Epoch {i + 1}: {loss:.4f}")
+
+    print("\nValidation Loss per Epoch:")
+    for i, loss in enumerate(val_losses):
+        print(f"Epoch {i + 1}: {loss:.4f}")
+
+# Dataset splitting
+def split_dataset(dataset_list, ratio=0.7):
+    random.shuffle(dataset_list)
+    split_idx = int(len(dataset_list) * ratio)
+    train_list = dataset_list[:split_idx]
+    val_list = dataset_list[split_idx:]
+    
+    return train_list, val_list
+
+# Define the transformations
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  
+    transforms.ToTensor()
+]) 
+train_data, validation_data = split_dataset(dataset_list, ratio=0.7)
+
+train_set = CustomDataset(train_data, transforms=transform)
+val_set = CustomDataset(validation_data, transforms=transform)
+
+train_loader = DataLoader(
+    train_set,
+    shuffle=True,
+    collate_fn=collate_fn
+)
+
+val_loader = DataLoader(
+    val_set,
+    shuffle=False,
+    collate_fn=collate_fn
+)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+num_classes = 2  
+num_epochs = 10
+train(model, train_loader, val_loader, num_epochs, device)

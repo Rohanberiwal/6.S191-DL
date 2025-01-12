1a78f05a81b0552530817d8957a8289e2e9f0f97
Atticus <119040957+Rohanberiwal@users.noreply.github.com>
2025-01-12 14:30:00 +0530

    Create installation_commands.py
    This is the command for the installation of the tiny yolo   version 4 .

---
+++ Latest_Updates/Yoloversion4/Tiny yolo/installation_commands.py
@@ -0,0 +1,4 @@
+!git clone https://github.com/hunglc007/tensorflow-yolov4.git
+!cd tensorflow-yolov4
+!# Download the YOLOv4-tiny weights
+!wget https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov4-tiny.weights
